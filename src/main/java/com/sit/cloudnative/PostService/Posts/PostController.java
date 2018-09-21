f4fa4a4ebfaeeb106309943b821f26cf766ac40b
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-09-21 22:12:46 +0700

    :clown_face: Mock user every create new post.

--- src/main/java/com/sit/cloudnative/PostService/Posts/PostController.java
+++ src/main/java/com/sit/cloudnative/PostService/Posts/PostController.java
@@ -1,5 +1,7 @@
 package com.sit.cloudnative.PostService.Posts;
 
+import com.sit.cloudnative.PostService.Users.User;
+import com.sit.cloudnative.PostService.Users.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,9 @@ import java.util.List;
 public class PostController {
     @Autowired
     private PostService postService;
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(
             method = RequestMethod.GET,
             value = "/posts"
@@ -29,6 +34,8 @@ public class PostController {
             value = "/posts"
     )
     public ResponseEntity<Post> createPost(@Valid @RequestBody Post post) {
+        User user = userService.getUserById(1);
+        post.setUser(user);
         Post post_object = postService.createPost(post);
         return new ResponseEntity<Post>(post_object,HttpStatus.OK);
     }
